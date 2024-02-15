2c9948af0ccb5a845a73d31a491afe2a16dfa3cb
Eloho <66059831+duske953@users.noreply.github.com>
2024-02-15 23:08:49 +0100

    update typo with backend url

--- src/utilities/utility.jsx
+++ src/utilities/utility.jsx
@@ -2,7 +2,7 @@ import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 
 // let url = "http://localhost:3000/api/v1";
-let url = import meta.env.VITE_BACKEND_URL
+let url = import.meta.env.VITE_BACKEND_URL
 
 export async function sendRequestToBackend(method, primaryRoute, data, route) {
   try {
